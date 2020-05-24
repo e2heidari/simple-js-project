41a44a95f8e3079933e1941a819cc86a39e8bce6
Ehsan <Ehsan.heydari@gmail.com>
2020-05-24 16:43:22 -0700

    put "ADD" button

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function handleOnClick() {
-  var number1 = prompt("Enter first number", "0");
+function add() {
+  var number1 = document.getElementById("num1").value;
   number1 = parseInt(number1);
-  var number2 = prompt("Enter second number", "0");
+  var number2 = document.getElementById("num2").value;
   number2 = parseInt(number2);
   var sum = number1 + number2;
-  // find the div
+
   var resultSumDiv = document.getElementById("resultSum");
-  // change the content with sum of num1 and num2
   resultSumDiv.innerText = sum;
 }
