66c8c47f3f0a9d633ff4364f5d2bedf10fea93ef
Ehsan <Ehsan.heydari@gmail.com>
2020-05-26 00:59:59 -0700

    add subtraction operation

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-function add() {
+function Process() {
   var number1 = document.getElementById("num1").value;
   number1 = parseInt(number1);
   var number2 = document.getElementById("num2").value;
   number2 = parseInt(number2);
   var sum = number1 + number2;
+  var minus = number1 - number2;
 
   var resultSumDiv = document.getElementById("resultSum");
   resultSumDiv.innerText = sum;
+  var resultMinusDiv = document.getElementById("resultMinus");
+  resultMinusDiv.innerText = minus;
+
 }
